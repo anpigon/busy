751a0430af98c08f264e0787579cf8a99175bcc7
anpigon <markan82@gmail.com>
2019-07-23 11:22:37 +0900

    modify ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,15 @@ module.exports = {
       instances: 4,
       exec_mode: 'cluster',
       max_memory_restart: '600M',
+      env: {
+        NODE_ENV: 'production',
+        STEEMCONNECT_CLIENT_ID: 'steemservice',
+        STEEMCONNECT_REDIRECT_URL: 'https://www.steemservice.com/callback',
+        STEEMCONNECT_HOST: 'https://steemconnect.com',
+        SIGNUP_URL: 'https://www.steempeople.com',
+        STEEMJS_URL: 'https://api.steemit.com',
+        // PORT: 3000,
+      },
     },
   ],
 };
